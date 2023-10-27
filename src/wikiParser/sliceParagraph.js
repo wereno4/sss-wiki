c8ed4ce70588d993230128c0ae25bf928eaa5fec
wereno4 <hellnow6312@gmail.com>
2023-10-27 17:24:55 +0900

    slicing paragraph

--- src/wikiParser/sliceParagraph.js
+++ src/wikiParser/sliceParagraph.js
@@ -1,6 +1,33 @@
 "use strict";
 
+let regex = [
+    /((?:=|=#) .+? (?:=|#=)\n)/ug,
+    /((?:==|==#) .+? (?:==|#==)\n)/ug,
+    /((?:===|===#) .+? (?:===|#===)\n)/ug,
+    /((?:====|====#) .+? (?:====|#====)\n)/ug,
+    /((?:=====|=====#) .+? (?:=====|#=====)\n)/ug,
+    /((?:======|======#) .+? (?:======|#======)\n)/ug
+];
+let count;
+
+function sliceRecursive (content) {
+    let separated = [];
+    while (true){
+        let stringPosition = temporaryStoredContent.slice(1).search(regex[count])+1;
+        if (stringPosition === 0 || stringPosition === -1) {
+            separated.push(temporaryStoredContent);
+            break;
+        }
+
+        separated.push(temporaryStoredContent.substring(0, stringPosition));
+        temporaryStoredContent = temporaryStoredContent.substring(stringPosition);        
+    
+    }
+
+    
+}
+
 module.exports = async content => {
-    let array = content.split(/((?:=|=#) .+? (?:=|#=)\n)/ug);
+
     
 };
--- src/wikiParser/tableTag.js
+++ src/wikiParser/tableTag.js
@@ -1,14 +1,5 @@
 "use strict";
 
 module.exports = content => {
-    let rawTables = content.match(/((?:\|\|.+\|\|\n)*)/ug);
-    let tables, result;
-    for (let rawTable of rawTables) {
-        //Making Tables
-    }
-    result = content;
-    for (let i in rawTables) {
-        result = result.replace(rawTables[i], tables[i]);
-    }
-    return result;
+    
 };
