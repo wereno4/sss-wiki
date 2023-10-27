2d14689f1e02e79f81521b34d16040c0b6e511ce
wereno4 <hellnow6312@gmail.com>
2023-10-28 02:12:40 +0900

    complete struct code

--- src/structContent/index.js
+++ src/structContent/index.js
@@ -1,3 +1,10 @@
 "use strict";
 
 const sliceParagraph = require('./sliceParagraph');
+const structParagraph = require('./structParagraph');
+
+module.exports = async content => {
+    let sliced = await sliceParagraph(content);
+    let result = await structParagraph(sliced);
+    return result;
+}
--- src/structContent/structParagraph.js
+++ src/structContent/structParagraph.js
@@ -1,10 +1,50 @@
 "use strict";
 
-let count = 0;
-
-function structRecursive (paramArray) {
-    
-}
-
 module.exports = async contentArray => {
-};
+
+    if (contentArray.length === 1) {
+       return contentArray[0];
+   }
+   let constructArray = contentArray;
+   for (let i = 5; i >= 0; i--) {
+       const currentIndexes = constructArray.map((element, index) => {
+           if (element.level === i) {
+               return index;
+           }
+       })
+       .filter(element => element >= 0);
+       if(currentIndexes.length === 0) {
+           if(i === 0) {
+               return {
+                   title: null,
+                   level: 0,
+                   hidden: false,
+                   child: constructArray
+               };
+           }
+           continue;
+       } else if (currentIndexes.length === 1) {
+           currentIndexes.push(constructArray.length);
+       }
+       for (let j = 0; j < currentIndexes.length; j++) {
+           if (currentIndexes[j] === constructArray.length || currentIndexes[j+1] - currentIndexes[j] <= 1) {
+               continue;
+           } else {
+               constructArray[currentIndexes[j]].child = [];
+               for (let k = currentIndexes[j]+1; k < currentIndexes[j+1]; k++) {
+                   if(constructArray[k].level <= i) {
+                       break;
+                   }
+                   constructArray[k].index = k - currentIndexes[j];
+                   constructArray[currentIndexes[j]].child.push(constructArray[k]);
+               }
+               if (constructArray[currentIndexes[j]].child.length === 0) {
+                delete constructArray[currentIndexes[j]].child;
+               }
+           }
+       }
+       constructArray = constructArray.filter(item => item.level <= i)
+   }
+ 
+   return constructArray[0];
+ };
