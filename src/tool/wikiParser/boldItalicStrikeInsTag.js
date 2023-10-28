eca56fa825da9c9660e1948f128b63dfb50890b5
wereno4 <hellnow6312@gmail.com>
2023-10-28 21:42:00 +0900

    remove folder

--- src/tool/wikiParser/boldItalicStrikeInsTag.js
+++ src/tool/wikiParser/boldItalicStrikeInsTag.js
@@ -4,4 +4,5 @@ module.exports = async content => content
                             .replace(/'''''(.+?)'''''/ug, '<strong><em>$1</em></strong>')
                             .replace(/'''(.+?)'''/ug, '<strong>$1</strong>')
                             .replace(/''(.+?)''/ug, '<em>$1</em>')
-                            .replace(/--(.+?)--/ug, '<del>$1</del>');
+                            .replace(/--(.+?)--/ug, '<del>$1</del>')
+                            .replace(/__(.+?)__/ug, '<ins>$1</ins>');
